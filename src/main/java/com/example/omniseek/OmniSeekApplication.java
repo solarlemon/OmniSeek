7d749428ebefedad8170af6dcbcf4340603dfb31
solar <shi13378957839@163.com>
2026-05-08 21:12:26 +0800

    init

---
+++ src/main/java/com/example/omniseek/OmniSeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.omniseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmniSeekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/omniseek/SmartPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omniseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniSeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
